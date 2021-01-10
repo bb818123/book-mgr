6288fed40fd5d78a67fc7100ce9a002a2cbff1f2
bb818123 <1303810429@qq.com>
2021-01-10 19:34:38 +0800

    分支测试

--- book-mgr-be/src/index.js
+++ book-mgr-be/src/index.js
@@ -3,35 +3,35 @@ const Koa = require('koa');
 
 const app = new Koa();
 
-//通过app.use注册中间件
-//中间件本质上 他就是一个函数
-//context 上下文-当前请求的相关信息都在里面
-app.use(async (context) => {
-    //对象的解构
-    const { request: req } = context;
-    const { url } = req;
+////通过app.use注册中间件
+////中间件本质上 他就是一个函数
+////context 上下文-当前请求的相关信息都在里面
+//app.use(async (context) => {
+//    //对象的解构
+//    const { request: req } = context;
+//    const { url } = req;
 
-    if (url == '/') {
-        context.response.body = '<h1>主页</h1>';
+//    if (url == '/') {
+//        context.response.body = '<h1>主页</h1>';
 
-        return;
-    }
+//        return;
+//    }
 
-    //路由
-    if (url == '/user/list') {
-        //访问数据库
-        context.response.body = '<h1>用户列表</h1>';
+//    //路由
+//    if (url == '/user/list') {
+//        //访问数据库
+//        context.response.body = '<h1>用户列表</h1>';
 
-        return;
-    }
+//        return;
+//    }
 
-    context.body = '404';
-    context.status = '404';
-});
+//    context.body = '404';
+//    context.status = '404';
+//});
 
-app.use((context) => {
-    context.body = '找不到资源';
-});
+//app.use((context) => {
+//    context.body = '找不到资源';
+//});
 
 app.listen(3000, () => {
     console.log('启动成功');
