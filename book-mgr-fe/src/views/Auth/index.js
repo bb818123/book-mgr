503356a421d262422b9a6d04abd2a06f7029401b
bb818123 <1303810429@qq.com>
2021-01-16 13:29:25 +0800

    完成大部分认证逻辑

--- book-mgr-fe/src/views/Auth/index.js
+++ book-mgr-fe/src/views/Auth/index.js
@@ -10,6 +10,13 @@ import {
 import {
   auth
 } from '@/service';
+import {
+  result
+} from '@/helpers/utils';
+import {
+  message
+} from 'ant-design-vue';
+
 
 
 export default defineComponent({
@@ -19,20 +26,76 @@ export default defineComponent({
     MailOutlined,
   },
   setup() {
+    //注册用的表单数据
     const regForm = reactive({
+      account: '',
+      password: '',
+      inviteCode:'',
+    });
+
+    //注册逻辑
+    const register = async () => {
+      if (regForm.account === '') {
+        message.info('请输入账户');
+        return;
+      }
+
+      if (regForm.password === '') {
+        message.info('请输入密码');
+        return;
+      }
+
+      if (regForm.inviteCode === '') {
+        message.info('请输入邀请码');
+        return;
+      }
+
+      const res = await auth.register(
+        regForm.account,
+        regForm.password,
+        regForm.inviteCode,
+        );
+
+      result(res)
+        .success((data) => {
+          message.success(data.msg);
+        });
+    };
+
+    //登录用的表单数据
+    const loginForm = reactive({
       account: '',
       password: '',
     });
 
-    const register = () => {
-      auth.register(regForm.account,regForm.password);
+    //登录逻辑
+    const login = async () => {
+      if (loginForm.account === '') {
+        message.info('请输入账户');
+        return;
+      }
+
+      if (loginForm.password === '') {
+        message.info('请输入密码');
+        return;
+      }
+
+      const res = await auth.login(loginForm.account, loginForm.password);
+
+      result(res)
+        .success((data) => {
+          message.success(data.msg);
+        });
     };
 
     return {
+      //注册相关数据
       regForm,
-
       register,
+
+      //登录相关数据
+      login,
+      loginForm,
     };
   },
 });
-
--- book-mgr-fe/src/views/Auth/index.vue
+++ book-mgr-fe/src/views/Auth/index.vue
@@ -12,7 +12,11 @@
       <a-tabs>
         <a-tab-pane key="1" tab="登录">
           <div class="item">
-            <a-input size="large" placeholder="账户">
+            <a-input
+             size="large"
+             placeholder="账户"
+             v-model:value="loginForm.account"
+             >
               <template v-slot:prefix>
                 <UserOutlined />
               </template>
@@ -20,7 +24,11 @@
           </div>
 
           <div class="item">
-            <a-input size="large" placeholder="密码">
+            <a-input
+             size="large"
+             placeholder="密码"
+             v-model:value="loginForm.password"
+             >
               <template v-slot:prefix>
                 <LockOutlined />
               </template>
@@ -32,7 +40,11 @@
           </div>
 
           <div class="item">
-            <a-button size="large" type="primary">
+            <a-button 
+            size="large"
+            type="primary"
+            @click="login"
+            >
               登录
             </a-button>
           </div>
@@ -63,7 +75,11 @@
           </div>
 
           <div class="item">
-            <a-input size="large" placeholder="邀请码">
+            <a-input 
+            size="large" 
+            placeholder="邀请码"
+            v-model:value="regForm.inviteCode"
+            >
               <template v-slot:prefix>
                 <MailOutlined />
               </template>
